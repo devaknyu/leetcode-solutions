071e473585c2208206a1e180f93bb269ec2739c7
devaknyu <ds8095@nyu.edu>
2025-09-17 23:28:02 -0400

    Approach and problem statement for question 27

--- Arrays/27_RemoveElement.py
+++ Arrays/27_RemoveElement.py
@@ -1,3 +1,24 @@
+"""
+LeetCode 27: Remove Element
+https://leetcode.com/problems/remove-element/
+
+Problem:
+Given an integer array nums and an integer val, remove all occurrences of val in-place. 
+The relative order of the elements may be changed.
+
+Return the number of elements in nums which are not equal to val.
+The first k elements of nums should contain the elements not equal to val.
+
+Approach:
+- Use a pointer `k` to track the next valid position in nums.
+- Iterate through nums:
+  - If nums[i] != val, copy nums[i] to nums[k] and increment k.
+- At the end, k will represent the number of valid elements.
+
+Time Complexity: O(n)   (single pass through array)
+Space Complexity: O(1)  (in-place, no extra data structure)
+"""
+
 from typing import List
 
 class Solution:
