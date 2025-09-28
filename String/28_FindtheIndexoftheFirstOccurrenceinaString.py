adb762511b17bc8eab0e85eec6ae35d4b3172ba5
devaknyu <ds8095@nyu.edu>
2025-09-27 23:27:05 -0400

    adding examples

--- String/28_FindtheIndexoftheFirstOccurrenceinaString.py
+++ String/28_FindtheIndexoftheFirstOccurrenceinaString.py
@@ -10,3 +10,9 @@ class Solution:
                 if j == len(needle) - 1:
                     return i
         return -1
+    
+    # Example run for local testing
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.strStr("sadbutsad", "sad"))   # Expected 0
+    print(sol.strStr("leetcode", "leeto"))  # Expected -1
