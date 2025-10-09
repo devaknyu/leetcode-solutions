2fb8ba805f64e8523ab25918655a445648b55010
devaknyu <ds8095@nyu.edu>
2025-10-08 22:53:26 -0400

    adding examples

--- Hash Table/268_MissingNumber.py
+++ Hash Table/268_MissingNumber.py
@@ -3,4 +3,9 @@ class Solution:
         res = len(nums)
         for i in range(len(nums)):
             res += i - nums[i]
-        return res
+        return res# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.missingNumber([3, 0, 1]))  # Expected output: 2
+    print(sol.missingNumber([0, 1]))     # Expected output: 2
+    print(sol.missingNumber([9,6,4,2,3,5,7,0,1]))  # Expected output: 8
