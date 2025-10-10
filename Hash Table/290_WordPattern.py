edefb6953adf4eec33460494a089b847781e9a9d
devaknyu <ds8095@nyu.edu>
2025-10-09 22:51:33 -0400

    adding examples

--- Hash Table/290_WordPattern.py
+++ Hash Table/290_WordPattern.py
@@ -16,3 +16,11 @@ class Solution:
             charToWord[c] = w
             wordToChar[w] = c
         return True
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.wordPattern("abba", "dog cat cat dog"))   # Expected output: True
+    print(sol.wordPattern("abba", "dog cat cat fish"))  # Expected output: False
+    print(sol.wordPattern("aaaa", "dog cat cat dog"))   # Expected output: False
+    print(sol.wordPattern("abba", "dog dog dog dog"))   # Expected output: False
