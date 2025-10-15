d95838c5ea02521358979a1210fa479c1e245b1d
devaknyu <ds8095@nyu.edu>
2025-10-15 19:42:53 -0400

    adding examples

--- Dynamic Programming/338_CountingBits.py
+++ Dynamic Programming/338_CountingBits.py
@@ -1,5 +1,5 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+    def countBits(self, n: int) -> list[int]:
         dp = [0] * (n + 1)
         offset = 1
 
@@ -9,3 +9,9 @@ class Solution:
             dp[i] = 1 + dp[i - offset]
         
         return dp
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.countBits(2))  # Expected output: [0, 1, 1]
+    print(sol.countBits(5))  # Expected output: [0, 1, 1, 2, 1, 2]
