b59df42e8e7bfe506aefedb7d15ab4dced93275d
devaknyu <ds8095@nyu.edu>
2025-10-18 20:51:25 -0400

    adding examples

--- Dynamic Programming/746_MinCostClimbingStairs.py
+++ Dynamic Programming/746_MinCostClimbingStairs.py
@@ -6,3 +6,8 @@ class Solution:
             dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1])
         return dp[n]
 
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minCostClimbingStairs([10, 15, 20]))        # Expected output: 15
+    print(sol.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))  # Expected output: 6
