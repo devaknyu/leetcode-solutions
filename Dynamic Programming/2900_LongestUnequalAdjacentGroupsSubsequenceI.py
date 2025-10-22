b58610439e8b92d137e0dbe89dc8610442e96d56
devaknyu <ds8095@nyu.edu>
2025-10-22 18:39:49 -0400

    adding examples

--- Dynamic Programming/2900_LongestUnequalAdjacentGroupsSubsequenceI.py
+++ Dynamic Programming/2900_LongestUnequalAdjacentGroupsSubsequenceI.py
@@ -5,3 +5,10 @@ class Solution:
             if groups[i] != groups[i - 1]:
                 res.append(words[i])
         return res
+    
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.getLongestSubsequence(["a", "b", "c", "d"], [1, 2, 2, 1]))  # Expected output: ["a", "b", "d"]
+    print(sol.getLongestSubsequence(["hello", "world"], [0, 1]))          # Expected output: ["hello", "world"]
+    print(sol.getLongestSubsequence(["x", "y", "z"], [1, 1, 1]))          # Expected output: ["x"]
