9d40dbeaddf7559947afc9c88d11ea9230f337b3
devaknyu <ds8095@nyu.edu>
2025-10-29 14:58:44 -0400

    adding examples

--- Sorting/628_MaximumProductofThreeNumbers.py
+++ Sorting/628_MaximumProductofThreeNumbers.py
@@ -8,3 +8,11 @@ class Solution:
             sorted_nums[0] * sorted_nums[-1] * sorted_nums[-2]
         )
         return product
+    
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maximumProduct([1, 2, 3]))            # Expected output: 6
+    print(sol.maximumProduct([1, 2, 3, 4]))         # Expected output: 24
+    print(sol.maximumProduct([-10, -10, 5, 2]))     # Expected output: 500
+    print(sol.maximumProduct([-1, -2, -3]))         # Expected output: -6
