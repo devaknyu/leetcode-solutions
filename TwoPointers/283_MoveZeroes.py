72db44ae116e471ee135850449933befa620db42
devaknyu <ds8095@nyu.edu>
2025-10-31 16:56:26 -0400

    adding examples

--- TwoPointers/283_MoveZeroes.py
+++ TwoPointers/283_MoveZeroes.py
@@ -11,3 +11,10 @@ class Solution:
                 nums[r] = temp
                 l += 1
         return nums
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.moveZeroes([0, 1, 0, 3, 12]))  # Expected output: [1, 3, 12, 0, 0]
+    print(sol.moveZeroes([0, 0, 1]))         # Expected output: [1, 0, 0]
+    print(sol.moveZeroes([4, 2, 4, 0, 0, 3, 0, 5, 1, 0]))  # Expected output: [4,2,4,3,5,1,0,0,0,0]
