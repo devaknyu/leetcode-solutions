eb58839e0199dd4f969388a9694a20258aa09633
devaknyu <ds8095@nyu.edu>
2025-11-17 18:05:15 -0500

    adding LeetCode 560: Subarray Sum Equals K

---
+++ Prefix Sum/560_SubarraySumEqualsK.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        curSum = 0
+        prefixSums = {0: 1}  # Base case: sum 0 appears once
+
+        for n in nums:
+            curSum += n
+            diff = curSum - k  # We've seen this prefix sum before
+            
+            res += prefixSums.get(diff, 0)
+            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+            
+        return res
