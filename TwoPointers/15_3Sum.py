cee266fa23778a924ebdeb6ad414c741151d47c4
devaknyu <ds8095@nyu.edu>
2025-11-26 19:49:52 -0600

    adding LeetCode 15: 3Sum

---
+++ TwoPointers/15_3Sum.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            # Skip duplicate elements for the first number
+            if i > 0 and nums[i-1] == a:
+                continue 
+            
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                three_sum = a + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    # Skip duplicates for the second number
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+        return res
