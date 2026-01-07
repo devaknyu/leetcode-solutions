151dd52c0757597df888c431c532f700a781e5d7
devaknyu <ds8095@nyu.edu>
2026-01-07 19:03:36 +0530

    adding LeetCode 1448: Count Good Nodes in Binary Tree

---
+++ Trees/1448_CountGoodNodesinBinaryTree.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        """
+        Returns the count of good nodes in the binary tree.
+        """
+
+        def dfs(node: Optional[TreeNode], maxval: int) -> int:
+            if not node:
+                return 0
+
+            # Check if current node is good
+            res = 1 if node.val >= maxval else 0
+
+            # Update max value for children
+            maxval = max(maxval, node.val)
+
+            # Continue DFS traversal
+            res += dfs(node.left, maxval)
+            res += dfs(node.right, maxval)
+
+            return res
+
+        return dfs(root, root.val)
