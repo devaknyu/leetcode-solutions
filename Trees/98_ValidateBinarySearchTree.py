059003c024d508976b93b87a6e6be1088fde27f1
devaknyu <ds8095@nyu.edu>
2026-01-08 18:13:46 +0530

    adding examples

--- Trees/98_ValidateBinarySearchTree.py
+++ Trees/98_ValidateBinarySearchTree.py
@@ -29,3 +29,18 @@ class Solution:
             )
 
         return dfs(root, float("-inf"), float("inf"))
+
+# Example usage
+if __name__ == "__main__":
+    """
+        Valid BST:
+            2
+           / \
+          1   3
+    """
+
+    root = TreeNode(2)
+    root.left = TreeNode(1)
+    root.right = TreeNode(3)
+
+    print(Solution().isValidBST(root))  # Expected: True
