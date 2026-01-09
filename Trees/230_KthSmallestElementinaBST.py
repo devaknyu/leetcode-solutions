27ec41407695c4eeebf60da3e0974fe8cbaba2aa
devaknyu <ds8095@nyu.edu>
2026-01-09 12:47:12 +0530

    adding examples

--- Trees/230_KthSmallestElementinaBST.py
+++ Trees/230_KthSmallestElementinaBST.py
@@ -33,3 +33,25 @@ class Solution:
 
             # Visit right subtree
             cur = cur.right
+
+# Example usage
+if __name__ == "__main__":
+    """
+        BST:
+              5
+             / \
+            3   6
+           / \
+          2   4
+         /
+        1
+    """
+
+    root = TreeNode(5)
+    root.left = TreeNode(3)
+    root.right = TreeNode(6)
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(4)
+    root.left.left.left = TreeNode(1)
+
+    print(Solution().kthSmallest(root, 3))  # Expected: 3
