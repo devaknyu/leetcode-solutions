9a2b19a8b4164cc55d62b5241e9d428300a0c64c
devaknyu <ds8095@nyu.edu>
2026-01-12 22:14:59 +0400

    adding LeetCode 297: Serialize and Deserialize Binary Tree

---
+++ Trees/297_SerializeandDeserializeBinaryTree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+    def serialize(self, root):
+        """
+        Encodes a binary tree into a string.
+        """
+
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        """
+        Decodes a serialized string back into a binary tree.
+        """
+
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
