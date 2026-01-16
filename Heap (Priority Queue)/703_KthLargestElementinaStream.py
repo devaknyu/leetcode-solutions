3631058d93f8f6410931ddf86911c98dbd59360e
devaknyu <ds8095@nyu.edu>
2026-01-16 19:26:18 +0400

    adding examples

--- Heap (Priority Queue)/703_KthLargestElementinaStream.py
+++ Heap (Priority Queue)/703_KthLargestElementinaStream.py
@@ -27,3 +27,12 @@ class KthLargest:
 
         # The root is the kth largest element
         return self.minHeap[0]
+
+# Example usage
+if __name__ == "__main__":
+    kthLargest = KthLargest(3, [4, 5, 8, 2])
+    print(kthLargest.add(3))   # 4
+    print(kthLargest.add(5))   # 5
+    print(kthLargest.add(10))  # 5
+    print(kthLargest.add(9))   # 8
+    print(kthLargest.add(4))   # 8
