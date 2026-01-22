fc715e87ec316f5bf2ccddcf2f888359d8a5e893
devaknyu <ds8095@nyu.edu>
2026-01-22 18:40:38 -0500

    adding LeetCode 295: Find Median from Data Stream

---
+++ Heap (Priority Queue)/295_FindMedianfromDataStream.py
@@ -0,0 +1,38 @@
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        # Max heap (simulated using negatives) for smaller half
+        self.small = []
+        # Min heap for larger half
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        # Add to max heap
+        heapq.heappush(self.small, -num)
+
+        # Maintain ordering: max(small) <= min(large)
+        if self.small and self.large and (-self.small[0] > self.large[0]):
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        # Rebalance sizes
+        if len(self.small) > len(self.large) + 1:
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -val)
+
+    def findMedian(self) -> float:
+        # Odd count
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+
+        # Even count
+        return (-self.small[0] + self.large[0]) / 2
