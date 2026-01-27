64ce37c61fa635807a52ded4efea99849abd3f4d
devaknyu <ds8095@nyu.edu>
2026-01-27 18:26:45 -0500

    adding LeetCode 90: Subsets II

---
+++ Backtracking/90_SubsetsII.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(i, subset):
+            # Reached the end → record subset
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+
+            # Include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1, subset)
+
+            # Backtrack
+            subset.pop()
+
+            # Skip all duplicates of nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+
+            # Exclude nums[i] and move forward
+            dfs(i + 1, subset)
+
+        dfs(0, [])
+        return res
