968900d33b4eaa30e1890b7a69c452fe22a43b04
devaknyu <ds8095@nyu.edu>
2026-01-26 19:46:11 -0500

    adding LeetCode 46: Permutations

---
+++ Backtracking/46_Permutations.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # Base case: one empty permutation
+        if len(nums) == 0:
+            return [[]]
+
+        # Get permutations of remaining elements
+        perms = self.permute(nums[1:])
+        res = []
+
+        # Insert nums[0] into every position of each permutation
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+
+        return res
