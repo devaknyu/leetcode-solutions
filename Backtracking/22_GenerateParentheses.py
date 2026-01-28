9128e874b04d5d26e9a161bf4acd636d5134849a
devaknyu <ds8095@nyu.edu>
2026-01-28 18:40:34 -0500

    adding LeetCode 22: Generate Parentheses

---
+++ Backtracking/22_GenerateParentheses.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+
+        def backtrack(openN, closedN):
+            # Valid complete sequence
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            # Add '(' if we still can
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            # Add ')' only if it keeps the sequence valid
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
