d8194d17cc8da3ea3893095918fd7a07cc8f9848
devaknyu <ds8095@nyu.edu>
2026-01-31 14:16:47 -0500

    adding LeetCode 17: Letter Combinations of a Phone Number

---
+++ Backtracking/17_LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(i, curStr):
+            # Completed one valid combination
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+
+            # Explore all letters for current digit
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
