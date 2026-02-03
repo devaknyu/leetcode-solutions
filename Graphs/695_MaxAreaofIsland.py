1eb222a0aefd1af4aeaef161d21290d2d32f2214
devaknyu <ds8095@nyu.edu>
2026-02-03 14:18:56 -0500

    adding LeetCode 695: Max Area of Island

---
+++ Graphs/695_MaxAreaofIsland.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+
+        def dfs(r, c):
+            # Out of bounds, water, or already visited
+            if (
+                r < 0 or r == rows or
+                c < 0 or c == cols or
+                (r, c) in visited or
+                grid[r][c] == 0
+            ):
+                return 0
+
+            visited.add((r, c))
+
+            # Count current cell + neighbors
+            return (
+                1 +
+                dfs(r + 1, c) +
+                dfs(r - 1, c) +
+                dfs(r, c + 1) +
+                dfs(r, c - 1)
+            )
+
+        area = 0
+        for r in range(rows):
+            for c in range(cols):
+                area = max(area, dfs(r, c))
+
+        return area
+
