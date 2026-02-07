43a6270531f9ac8191bee91766f9ccb597cef600
devaknyu <ds8095@nyu.edu>
2026-02-06 23:50:52 -0500

    adding LeetCode 994: Rotting Oranges

---
+++ Graphs/994_RottingOranges.py
@@ -0,0 +1,38 @@
+from typing import List
+import collections
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        time = 0
+        fresh = 0
+        q = collections.deque()
+        ROWS, COLS = len(grid), len(grid[0])
+
+        # Initialize fresh count and queue with all rotten oranges
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
+        # BFS traversal
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (
+                        row < 0 or row >= ROWS or
+                        col < 0 or col >= COLS or
+                        grid[row][col] != 1
+                    ):
+                        continue
+                    grid[row][col] = 2
+                    q.append((row, col))
+                    fresh -= 1
+            time += 1
+
+        return time if fresh == 0 else -1
