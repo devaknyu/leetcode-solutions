0e4291f172ed1a4b388e1354094ec4eb2de81e75
devaknyu <ds8095@nyu.edu>
2026-02-07 20:38:50 -0500

    adding examples

--- Graphs/417_PacificAtlanticWaterFlow.py
+++ Graphs/417_PacificAtlanticWaterFlow.py
@@ -38,3 +38,15 @@ class Solution:
                     res.append([r, c])
 
         return res
+    
+    # Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    heights = [
+        [1,2,2,3,5],
+        [3,2,3,4,4],
+        [2,4,5,3,1],
+        [6,7,1,4,5],
+        [5,1,1,2,4]
+    ]
+    print(sol.pacificAtlantic(heights))
