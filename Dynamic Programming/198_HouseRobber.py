3a12fb540ac63a3dd9970c4591c1d5e82dbad78d
devaknyu <ds8095@nyu.edu>
2026-02-17 22:44:22 -0500

    adding LeetCode 198: House Robber

---
+++ Dynamic Programming/198_HouseRobber.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            tmp = max(rob2, rob1 + n)
+            rob1 = rob2
+            rob2 = tmp
+
+        return rob2
