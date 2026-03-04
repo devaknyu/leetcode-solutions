481658ab535900f7e81e44888e9e0c144c2c21ef
devaknyu <ds8095@nyu.edu>
2026-03-03 22:32:26 -0500

    adding examples

--- Intervals/253_MeetingRoomsII.py
+++ Intervals/253_MeetingRoomsII.py
@@ -4,7 +4,7 @@ class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        
+
 class Solution:
     def minMeetingRooms(self, intervals: List[Interval]) -> int:
         if not intervals:
@@ -27,3 +27,7 @@ class Solution:
             res = max(res, cur)
 
         return res
+
+sol = Solution()
+intervals = [Interval(0,30), Interval(5,10), Interval(15,20)]
+print(sol.minMeetingRooms(intervals))  # Expected: 2
