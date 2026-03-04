d5b7bea4470f05b32053687b4cb9bc1cfd271e84
devaknyu <ds8095@nyu.edu>
2026-03-03 22:31:31 -0500

    adding LeetCode 253: Meeting Rooms II

---
+++ Intervals/253_MeetingRoomsII.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        s, e = 0, 0
+        res, cur = 0, 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                cur += 1
+            else:
+                e += 1
+                cur -= 1
+
+            res = max(res, cur)
+
+        return res
