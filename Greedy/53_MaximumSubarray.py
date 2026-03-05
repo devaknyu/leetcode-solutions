44db718a34159e7440106831efcd1a8e9fd89503
devaknyu <ds8095@nyu.edu>
2026-03-04 22:35:14 -0500

    adding examples

--- Greedy/53_MaximumSubarray.py
+++ Greedy/53_MaximumSubarray.py
@@ -12,3 +12,10 @@ class Solution:
             maxSub = max(maxSub, curSum)
 
         return maxSub
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))  # Expected: 6
+    print(sol.maxSubArray([1]))                     # Expected: 1
+    print(sol.maxSubArray([5,4,-1,7,8]))            # Expected: 23
