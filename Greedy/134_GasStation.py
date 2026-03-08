4e4083932f679a7222e91dd8a1ad0eef73849522
devaknyu <ds8095@nyu.edu>
2026-03-07 22:05:04 -0500

    adding examples

--- Greedy/134_GasStation.py
+++ Greedy/134_GasStation.py
@@ -15,3 +15,9 @@ class Solution:
                 res = i + 1
 
         return res
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))  # Expected: 3
+    print(sol.canCompleteCircuit([2,3,4], [3,4,3]))          # Expected: -1
