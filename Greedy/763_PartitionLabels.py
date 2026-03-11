8864c54d3711f290ff68ac59b4103b4e22269ad8
devaknyu <ds8095@nyu.edu>
2026-03-10 23:07:59 -0400

    adding examples

--- Greedy/763_PartitionLabels.py
+++ Greedy/763_PartitionLabels.py
@@ -19,3 +19,9 @@ class Solution:
                 size = 0
 
         return res
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.partitionLabels("ababcbacadefegdehijhklij"))  # Expected: [9,7,8]
+    print(sol.partitionLabels("eccbbbbdec"))                 # Expected: [10]
