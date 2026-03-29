87f70943880a76657e535bac8552911a76c7fe25
devaknyu <ds8095@nyu.edu>
2026-03-29 17:45:07 -0400

    adding Practice question

---
+++ Practice/5_ValidPalindrome.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        char = list(s)
+        left = 0
+        right = len(char) - 1
+        
+        while left < right:
+            while left < right and not self.AlphaNum(char[left]):
+                left += 1
+            while left < right and not self.AlphaNum(char[right]):
+                right -= 1
+            if char[left].lower() != char[right].lower():
+                return False
+            left += 1
+            right -= 1
+        
+        return True
+
+    def AlphaNum(self, c: str) -> bool:
+        return (ord('0') <= ord(c) <= ord('9')) or (ord('a') <= ord(c.lower()) <= ord('z'))
