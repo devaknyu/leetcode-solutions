b61ed8137d8fa09ff923f31909256f6e42de96bc
Devak Somaraj <ds8095@nyu.edu>
2026-07-07 20:00:34 -0400

    adding Practice question-example

--- Practice/01_ContainsDuplicate.py
+++ Practice/01_ContainsDuplicate.py
@@ -7,3 +7,10 @@ class Solution:
             seen.add(i)
         return False
 
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.containsDuplicate([1, 2, 3, 1]))  # Expected output: True
+    print(sol.containsDuplicate([1, 2, 3, 4]))  # Expected output: False
+    print(sol.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  # Expected output: True
