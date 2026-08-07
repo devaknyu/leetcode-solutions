8f39aacdc0cafd146b73385776b690301ff681e7
Devak Somaraj <ds8095@nyu.edu>
2026-08-06 20:21:02 -0400

    adding Practice question-approach in Java

--- Java_Practice/01_ContainsDuplicate.java
+++ Java_Practice/01_ContainsDuplicate.java
@@ -1,3 +1,21 @@
+/*
+LeetCode 217: Contains Duplicate
+https://leetcode.com/problems/contains-duplicate/
+
+Approach:
+
+- Use a HashSet to store all unique elements.
+- Iterate through each number in the array:
+  - Add each number to the HashSet.
+- After processing all elements:
+  - If the size of the HashSet is smaller than the length of the array,
+    duplicates exist, so return true.
+  - Otherwise, return false.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> res = new HashSet<>();
@@ -7,3 +25,4 @@ class Solution {
         return res.size() != nums.length;
     }
 }
+
